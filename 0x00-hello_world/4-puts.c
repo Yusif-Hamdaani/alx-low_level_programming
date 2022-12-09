19d3df91e38a92013097b837a9e6e5a42cb2f907
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2022-12-08 18:53:02 -0800

    A C program that prints exactly Programming is like buildi ng a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - prints out a string via puts
  * Description: Program that prints out the string s followed by a new line
  * Return: int 0 when done and no errors
- * */
+ **/
 
 int main(void)
 {
