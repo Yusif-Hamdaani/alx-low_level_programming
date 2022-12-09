8c0815dc9896c339182ecb1de63998ec78254716
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2022-12-08 18:32:04 -0800

    A C program that prints exactly Programming is like buildi ng a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  * * main - prints out a string via puts
@@ -8,8 +9,10 @@
  * * Return: int 0 when done and no errors
  *
  * */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
+
 }
