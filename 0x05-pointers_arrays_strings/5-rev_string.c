46430a7b1fc415bd6ffdc94c1cb70751e4b4dad6
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2022-12-19 22:30:30 -0800

    A function that reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,11 +2,9 @@
 
 /**
  * rev_string - reverses a string
- *
  * _putchar - print each character
  * @s: char to check
- * 
- * Description: This will reverse a string
+ *
  * Return: 0 is success
 */
 
