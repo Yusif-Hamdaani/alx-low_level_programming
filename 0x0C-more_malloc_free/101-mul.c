b37ee1aba3fa82b65e4569bc40700fda216f43fc
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-01-13 12:46:29 -0800

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,72 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <ctype.h>
 
-/**
- * main - multiplies two positive numbers.
- * @argc: number of arguments
- * @argv: the values in each argc element
- *
- * Description: its like infinite add but with multiplication
- * Return: print the result. return 0 if success
-*/
-
-# IF EITHER NUMBER ARE ZERO RETURN 0;
-##SHOULD CHECK IF BEGINNING LEADING NUMBER IS ZERO.
-
-
-int main(int argc, char **argv)
-{
-	int size1 = 0;
-	int size2 = 0;
-	int product_size = 0;
-	int product = 0;
-	int overflow = 0;
-	int index = 0;
-	int tab = 0;
-	while (argv[1][size1])//strlength and checks if digit
-	{
-		ISDIGIT\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\##################
-		size1++;
+int main(int argc, char *argv[]) {
+	// Check for the correct number of arguments
+	if (argc != 3) {
+		printf("Error\n");
+		exit(98);
 	}
-	while (argv[2][size2])//strlength and checks if digit
-	{
-		ISITADIGIT########################################################
-		size2++;
-	}
-	product_size = size1 + size2 + 1;//calculate the range we need
-	product = malloc(sizeof(char) * product_size);//allocates space
-	if (!product)
-		return (98);
-	while (product_size >= 0)//initialize all to 0
-		product[product_size] = '0';
-	size1--;//because length is 1 more than index
-	size2--;
-	while (size2 >= 0)
-	{
-		index = tab;
-		for (j = size1; j >= 0; j--)
-		{
-			product = (argv[1][j]-'0') * (argv[2][size2]-'0');
-			product += product[index] -'0';
-			product += overflow;
-			overflow = product / 10;
-			product[index] = product % 10 + '0';
-			index++;
+
+	// Check if the arguments are composed of digits
+	for (int i = 1; i < argc; i++) {
+		for (int j = 0; argv[i][j]; j++) {
+			if (!isdigit(argv[i][j])) {
+				printf("Error\n");
+				exit(98);
+			}
 		}
-		if (overflow)
-		{
-			product[index] = overflow;
-			index++;
-		}
-		overflow = 0;
-		size2--;
-		tab++;
 	}
-	product[index] ='\0';
 
-	reversestring\\\\\###########################################################
-		printf("%s\n", product);
-	free(product);
-	return (0);
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+	printf("%d\n", result);
+	return 0;
 }
