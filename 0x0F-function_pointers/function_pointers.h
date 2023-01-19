01b642aadcd2085acd1e582ae50237c79a19fe50
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-01-19 03:10:00 +0000

    A function that prints a name.

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,13 @@
+#ifndef _FUNCTIONS_POINTERS_H
+#define _FUNCTIONS_POINTERS_H
+#include <stddef.h>
+/* stddef.h header defines various types and macros */
+/* size_t is unsigned integral type and is the result of the sizeof keyword */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
+#endif
