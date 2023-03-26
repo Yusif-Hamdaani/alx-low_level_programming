335bc2a3b8dee2913b22962976b5be177a2cd115
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-03-26 07:53:06 -0700

    dynamic_libraries Task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
+int div(int a, int b)
+{
+	return(a / b);
+}
+int mod(int a, int b)
+{
+	return(a % b);
+}
